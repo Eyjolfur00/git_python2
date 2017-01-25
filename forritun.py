35b622039b69d6c6939f1eb4ae029f23e8b44082
Eyjolfur00 <eyjolfurar268@nemi.tskoli.is>
2017-01-25 11:15:30 +0000

    Daemi1

--- forritun.py
+++ forritun.py
@@ -1,2 +1,11 @@
 #eyjolfur
-print("konni")
+
+#Liður 1
+
+tala1 = int(input("Sláðu inn tölu"))
+tala2 = int(input("Sláðu inn aðra tölu"))
+marg = tala1 + tala2
+bla = tala1 * tala2
+print("tölurnar plúsaðar",tala1,"+", tala2,"=", marg)
+print("tölurnar sinnumaðar",tala1,"*", tala2,"=", bla)
+
