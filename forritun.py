31b06835f42d814681c775e21fca4b5f72812026
Eyjolfur00 <eyjolfurar268@nemi.tskoli.is>
2017-01-25 11:16:09 +0000

    Daemi2

--- forritun.py
+++ forritun.py
@@ -9,3 +9,8 @@ bla = tala1 * tala2
 print("tölurnar plúsaðar",tala1,"+", tala2,"=", marg)
 print("tölurnar sinnumaðar",tala1,"*", tala2,"=", bla)
 
+#liður 2
+print("#Liður 2")
+ta1 = input("gefðu mér Fornafnið þitt")
+ta2 = input("gefðu mér eftirnafnið þitt")
+print("hello", ta1, ta2)
